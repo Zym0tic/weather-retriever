a0555fb11243add4b7dfe25bb7bb23eb53b92d56
zym0tic <lukerozon@gmail.com>
2022-07-31 23:46:54 -0400

    began adding Onecall and geolocator APIs

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,12 @@
 var APIKey = "fffd659f7674c5ce9fb2ce87ff19d10f";
 var cityFormEl = document.querySelector('#city-form');
 var cityInputEl = document.querySelector('#cityname');
+var cityInfoEl = document.querySelector('#cityInfo');
+var temp = document.querySelector('.temp');
+var wind = document.querySelector('.wind');
+var humidity = document.querySelector('.humidity');
+var uv = document.querySelector('.uv');
+var icon = document.querySelector('.weather-icon');
 
 
 var formSubmitHandler = function (event) {
@@ -9,9 +15,10 @@ var formSubmitHandler = function (event) {
     var cityName = cityInputEl.value.trim();
   
     if (cityName) {
-      getWeatherRepos(cityName);
+      getWeather(cityName);
   
     //   repoContainerEl.textContent = '';
+
       cityInputEl.value = '';
     } else {
       alert('Please enter the name of a city');
@@ -19,26 +26,62 @@ var formSubmitHandler = function (event) {
   };
 
 
-var getWeatherRepos = function (city) {
-    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+// var getWeather = function (city) {
+//     var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-    fetch(apiUrl)
+
+//     fetch(apiUrl)
+//     .then(function (response) {
+//       if (response.ok) {
+//         console.log(response);
+//         response.json().then(function (data) {
+//           console.log(data);
+
+
+        
+//           displayWeather(data, city);
+        
+//         });
+//       } else {
+//         alert('Error: ' + response.statusText);
+//       }
+//     })
+//     .catch(function (error) {
+//       alert('Unable to connect to OpenWeather');
+//     });
+
+// } 
+
+var getWeather = function (city) {
+    var cityUrl = "https://api.openweathermap.org/data/3.0/onecall?lat=" + lat + "&lon=" + lon + "&exclude=" +part +"&appid=" + APIKey;
+    
+    fetch(cityUrl)
     .then(function (response) {
       if (response.ok) {
         console.log(response);
         response.json().then(function (data) {
           console.log(data);
-        //   displayRepos(data, user);
-        });
-      } else {
-        alert('Error: ' + response.statusText);
-      }
-    })
-    .catch(function (error) {
-      alert('Unable to connect to OpenWeather');
-    });
 
-}
+          fetch ("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit={limit}&appid=" + APIKey)
+          .then(function(response) {
+            return response.json();
+          })
+          .then(function (data){
+            console.log(data);
+            displayWeather(data, city);
+          });
+
+});
+
+var displayWeather = function (data) {
+    cityInfoEl.textContent = data.name + " (" + moment().format("MMM Do YY") + ")";
+    temp.textContent = "Temp: " + (Math.round(data.main.temp * 9 / 5 - 459.67)) + "°F";
+    wind.textContent = "Wind: " + data.wind.speed + " MPH";
+    humidity.textContent = "Humidity: " + data.main.humidity + "%";
+    // uv.textContent = "UV Index: " + ;
+
+};
+
 
 
 
